b3476a205d7c306222af097cbd37e870ad57bc60
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2023-10-19 08:32:39 -0400

    Update KmTOMiles2.java

--- KmTOMiles2.java
+++ KmTOMiles2.java
@@ -10,6 +10,13 @@ class Program1 extends ConsoleProgram {
   
   public void run() {
 
+     double dblMiles;
+
+
+    for (int intKm = 10; intKm <= 100; intKm += 10){
+      dblMiles = intKm * 0.621371;
+      System.out.println(intKm + "km --> " + dblMiles + "mi");
+
    
     
   }
