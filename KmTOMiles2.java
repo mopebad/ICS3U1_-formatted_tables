ad4fb9bf2c8de3b3f7ee28e7e1a9e8314a666a1c
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2023-10-19 08:32:17 -0400

    Update and rename Program1.java to KmTOMiles2.java

---
+++ KmTOMiles2.java
@@ -0,0 +1,16 @@
+class Program1 extends ConsoleProgram {
+
+/**
+*Given a number n, calculate the sum of the numbers from 1 to n.
+*
+* @author: E. Fabroa
+*
+*/
+
+  
+  public void run() {
+
+   
+    
+  }
+}
